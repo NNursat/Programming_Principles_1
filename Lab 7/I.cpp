d236bdcee831cb22ed50857588e09fe3f31b6100
m0nteroskywalker <salau1977@gmial.com>
2024-11-01 05:36:18 +0500

    I

---
+++ Lab 7/I.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a;
+
+    vector<int> arr_int;
+    vector<string> arr_str(a, "YES");
+
+    for (int i = 0; i < a; i++)
+    {
+        cin >> b;
+        arr_int.push_back(b);
+    }
+
+    for (int i = 0; i < a; i++)
+    {
+        for (int ii = i + 1; ii < a; ii++)
+        {
+            if (arr_int[i] == arr_int[ii])
+            {
+                arr_str[ii] = "NO";
+            }
+            
+        }
+        
+    }
+
+    for (string i : arr_str) {
+        cout << i << endl;
+    }
+
+
+}
